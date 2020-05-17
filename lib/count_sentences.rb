fe34d7e167065798f7d0732c355e14571fbac696
Teinelle Charles <teinelledcharles@gmail.com>
2020-05-17 08:47:12 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,6 +27,7 @@ class String
   end
 
   def count_sentences
-   new_array= self.split(/[.?!]).reject {|string| string.empty?}
+   new_array= self.split(/[.?!]/).reject {|string| string.empty?}
+   retun new_array.length
   end
 end
